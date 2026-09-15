534d37a1e79ddd9a5d8106486104ba3026301006
Alvaro Andres <alvaroandres2802@gmail.com>
2026-09-15 17:02:41 -0500

    Disable 12h rule so Andy can test cron at 6 PM

--- api/reminders.js
+++ api/reminders.js
@@ -120,12 +120,15 @@ export default async function handler(req, res) {
     if (!cita.customers?.phone) continue
     
     // Skip sending 24h reminder if the appointment was created less than 12 hours ago
+    // [QA OVERRIDE]: Temporarily disabled so Andy can test the cron job at 6 PM
+    /*
     const createdDate = new Date(cita.created_at)
     if (now.getTime() - createdDate.getTime() < 12 * 60 * 60 * 1000) {
       // Mark as sent so it doesn't try again, but don't actually send it to avoid spam
       await supabase.from('appointments').update({ reminder_24h_sent: true }).eq('id', cita.id)
       continue
     }
+    */
 
     const hora = new Date(cita.scheduled_at).toLocaleTimeString('es-EC', { timeZone: 'America/Guayaquil', hour: '2-digit', minute: '2-digit' })
 
